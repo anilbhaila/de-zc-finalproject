bbc9827072e48ec7ef62b6261ffb4fd6fdf2bf0f
anilbhaila <anil.bhaila.2020@gmail.com>
2026-03-23 17:16:37 +0000

    Terraform apply successfuly created GCP resources

---
+++ terraform/main.tf
@@ -0,0 +1,237 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.19.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials)
+  project     = var.project
+  region      = var.region
+}
+
+# GCS Data Lake Storage Bucket
+resource "google_storage_bucket" "ny_traffic_events_bucket" {
+  name          = var.gcs_bucket_name
+  location      = var.location
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+
+  lifecycle_rule {
+    condition {
+      age = 1  # Keep for 30 days
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
+
+# Create a directory for temporary files of Dataflow
+resource "google_storage_bucket_object" "dataflow_temp_folder" {
+  name    = "temp/"
+  content = "temp folder for dataflow"
+  bucket  = google_storage_bucket.ny_traffic_events_bucket.name
+}
+
+# Create a directory for Dataflow staging files
+resource "google_storage_bucket_object" "dataflow_staging_folder" {
+  name    = "staging/"
+  content = "staging folder for dataflow"
+  bucket  = google_storage_bucket.ny_traffic_events_bucket.name
+}
+
+# BigQuery Dataset - Raw Data
+resource "google_bigquery_dataset" "raw_dataset" {
+  dataset_id = var.raw_dataset_name
+  location   = var.location
+}
+
+# BigQuery Dataset - Processed Data
+resource "google_bigquery_dataset" "processed_dataset" {
+  dataset_id = var.processed_dataset_name
+  location   = var.location
+}
+
+# BigQuery raw data table
+resource "google_bigquery_table" "ny_traffic_events_table" {
+  dataset_id = google_bigquery_dataset.raw_dataset.dataset_id
+  table_id   = "ny_traffic_events"
+  deletion_protection = false
+
+  time_partitioning {
+    type  = "DAY"
+    field = "LastUpdated"
+  }
+
+  schema = <<EOF
+[
+  {
+    "name": "ID",
+    "type": "STRING",
+    "mode": "REQUIRED"
+  },
+  {
+    "name": "RegionName",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "Severity",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "RoadwayName",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "DirectionOfTravel",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "Description",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "Location",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "LanesAffected",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "LanesStatus",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "PrimaryLocation",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "SecondaryLocation",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "FirstArticleCity",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "SecondCity",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "EventType",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "EventSubType",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "MapEncodedPolyline",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "LastUpdated",
+    "type": "TIMESTAMP",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "Latitude",
+    "type": "FLOAT",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "Longitude",
+    "type": "FLOAT",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "PlannedEndDate",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "Reported",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "StartDate",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "Schedule",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "ScheduleScheduleId",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "ScheduleStart",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "ScheduleEnd",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "ScheduleContinuous",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "ScheduleActiveDays",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "ScheduleImpact",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "ingestion_time",
+    "type": "TIMESTAMP",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "processing_time",
+    "type": "TIMESTAMP",
+    "mode": "NULLABLE"
+  }
+]
+EOF
+}
+
+# Load Dataflow job script into GCS
+resource "google_storage_bucket_object" "dataflow_job_file" {
+  name   = "jobs/kafka_to_gcs_pipeline.py"
+  bucket = google_storage_bucket.ny_traffic_events_bucket.name
+  source = "../src/dataflow/kafka_to_gcs_pipeline.py"
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,58 @@
+variable "credentials" {
+    description = "GCP Credentials"
+    # Replace with your credential path
+    default = ".keys/gcp-creds.json"
+}
+
+variable "project" {
+    description = "GCP Project ID"
+    # Replace with your project ID
+    default = "dtc-ab-de-2026"
+}
+
+variable "region" {
+    description = "GCP Region"
+    # Replace with your location
+    default = "us-south1"
+}
+
+variable "location" {
+    description = "Data Location"
+    default = "us-south1"
+}
+
+variable "raw_dataset_name" {
+  description = "BigQuery Raw Dataset Name"
+  default     = "ny_traffic_events_raw"
+}
+
+variable "processed_dataset_name" {
+  description = "BigQuery Processed Dataset Name"
+  default     = "ny_traffic_events_processed"
+}
+
+variable "gcs_bucket_name" {
+    description = "Data Lake Bucket Name"
+    # bucket name
+    default = "ny_traffic_events"
+}
+
+variable "gcs_storage_class" {
+    description = "Bucket Storage Class"
+    default = "STANDARD"
+}
+
+variable "dataproc_cluster_name" {
+  description = "Dataproc Cluster Name"
+  default     = "ny_traffic_events-flink-cluster"
+}
+
+variable "dataproc_machine_type" {
+  description = "Machine type for Dataproc cluster nodes"
+  default     = "n1-standard-2"
+}
+
+variable "dataproc_image_version" {
+  description = "Dataproc image version"
+  default     = "2.1-debian10"
+}
