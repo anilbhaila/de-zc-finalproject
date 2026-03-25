0eff16d89a463674c2eaf0e62f1f1ebaca3fa60e
hong sau <hong.sau.2024@gmail.com>
2026-03-25 23:32:18 +0000

    Added DataRefreshedDate in rpt_ny_traffic_event_county.sql

--- dbt/models/reporting/rpt_ny_traffic_event_county.sql
+++ dbt/models/reporting/rpt_ny_traffic_event_county.sql
@@ -5,12 +5,18 @@
 }}
 
 SELECT
+  {{ dbt_date.convert_timezone("ingestion_time", target_tz="America/Chicago") }} as DataRefreshedDate,
   EventType,
   COALESCE(County, 'Not Specified') AS County,
   COUNT(*) AS data_points
 FROM 
   {{ ref('stg_ny_traffic_events') }}
 GROUP BY
+  DataRefreshedDate,
   EventType,
   COALESCE(County, 'Not Specified')
+  
+ORDER BY
+  DataRefreshedDate,
+  data_points DESC
   
